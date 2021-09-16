7c12a7c777a8864bca538f272f051234599e5ec3
= <=>
2021-09-16 09:52:29 -0300

    add gulp

---
+++ gulpfile.ts
@@ -0,0 +1,14 @@
+import gulp from 'gulp'
+import shell from 'gulp-shell'
+import replace from 'gulp-replace'
+
+const compile = () => gulp.src('package.json').pipe(shell('npm run compile'))
+
+const copy = () => gulp.src(['readme.md', 'src/**/*.ts']).pipe(gulp.dest('dist'))
+
+const update = () =>
+  gulp.src('package.json').pipe(replace('"private": true,', '"private": false,')).pipe(gulp.dest('dist'))
+
+const publish = () => gulp.src('package.json').pipe(shell('cd dist && npm publish'))
+
+export default gulp.series(compile, copy, update, publish)
